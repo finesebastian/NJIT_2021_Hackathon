1df6168598e75cc808823243876bc6323ab05928
finesebastian <snf5@njit.edu>
2021-11-14 09:50:05 -0500

    Included certs for SSL

--- get_package_log.php
+++ get_package_log.php
@@ -1,21 +1,32 @@
 <?php
-    try {
-        $dbh = new PDO('pgsql:host=free-tier.gcp-us-central1.cockroachlabs.cloud;
-            port=26257;
-            dbname=hacknjit2021-4844.defaultdb;
-            sslmode=full verification;
-            sslrootcert=certs/root.crt',
-            'ayushi', yYuKthSWBJly_k4o, array(
-                PDO::ATTR_ERRMODE          => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_EMULATE_PREPARES => true,
-                PDO::ATTR_PERSISTENT => true
-            ));
-        $id_number = ';0000000000000218529/?';
-        $id_number = $_REQUEST["q"];
-        $stmnt = "SELECT recipient_first_name, recipient_last_name, package_id,package_location, package_status FROM packages WHERE packages.recipient_id_number LIKE '$id_number'";;
-        echo $dbh->query($stmnt);
-    } catch (Exception $e) {
-        print "ITS NOT WORKING";
+    $db_return_value="";
+//    $dbh = new PDO('pgsql:host=localhost;
+//    port=26257;
+//    dbname=hacknjit2021-4844.defaultdb;
+//    sslmode=full_verification;
+//    sslrootcert=/certs/ca.crt;',
+//    'ayushi', 'yYuKthSWBJly_k4o', array(
+//        PDO::ATTR_ERRMODE          => PDO::ERRMODE_EXCEPTION,
+//        PDO::ATTR_EMULATE_PREPARES => true,
+//        PDO::ATTR_PERSISTENT => true
+//    ));
+
+    $id_number = $_COOKIE["q"];
+    $servername ="free-tier.gcp-us-central1.cockroachlabs.cloud";
+    $username ="ayushi";
+    $password = "yYuKthSWBJly_k4o";
+    $dbname = "hacknjit2021-4844.defaultdb";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    $db_return_value = $dbh->query("SELECT * from packages WHERE packages.recipient_id_number LIKE'$id_number'");
+    while($row = $result->fetch_assoc()) {
+        foreach ($row as $k=>$v)
+        {
+            $db_return_value+= "$k : $v";
+            $db_return_value += "<br />";
+        }
     }
+    echo $id_number;
 
 ?>
